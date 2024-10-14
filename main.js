ebe73ff6c9092ce79ef8a61215fa740980bcde21
metascifi <177558001+metascifi@users.noreply.github.com>
2024-10-14 13:15:59 +0300

    Update: Add interactive dropdown
    Also fix minor bugs such as not clearing checkbox, staying of theaders when table is clear;

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ let contentContainer = document.querySelector(".book-section")
 let btnNewBook = document.querySelector("#newBook");
 let warningText = document.createElement("p");
 let tableRow = document.createElement("tr");
+let dropdownContainer = document.createElement("div");
+let dropdown = document.createElement("div");
+let optionOne = document.createElement("p");
+let optionTwo = document.createElement("p");
+let checkConfirmation;
+
 
 
 function Book(name, author, year, pages, isRead) {
@@ -18,12 +24,14 @@ function Book(name, author, year, pages, isRead) {
 
 function addBookToLibrary(name, author, year, pages, isRead) {
     if (isRead.checked === true){
-        isRead = "Completed"
+        checkConfirmation = "Completed"
     } else {
-        isRead = "Not finished"
+        checkConfirmation = "Not finished"
     };
-    let BookInst = new Book(name, author, year, pages, isRead);
+    let BookInst = new Book(name, author, year, pages, checkConfirmation);
     myLibrary.push(BookInst);
+    dropdownContainer = document.createElement("div");
+    BookInst.isRead = dropdownContainer; 
     return BookInst;
 }
 
@@ -129,7 +137,20 @@ btnNewBook.addEventListener("click", (event) => {
          for (let i = 0; i < properties.length; i++){
               tableCell = document.createElement("td");
               tableRow.appendChild(tableCell);
+              if ((typeof addedBook[properties[i]]) === "string") {
               tableCell.textContent = addedBook[properties[i]];
+              } else if ((typeof addedBook[properties[i]]) === "object") {
+                tableCell.appendChild(addedBook[properties[i]]);
+                dropdown = document.createElement("div");
+                optionOne = document.createElement("p");
+                optionTwo = document.createElement("p");
+                dropdownContainer.setAttribute("class", "dropdown-container");
+                dropdownContainer.appendChild(dropdown);
+                dropdown.setAttribute("class", "dropdown");
+                dropdown.appendChild(optionOne);
+                dropdown.appendChild(optionTwo);
+                addedBook[properties[i]] = checkConfirmation;
+              }
           };
 
           // Creates remove button 
@@ -139,8 +160,12 @@ btnNewBook.addEventListener("click", (event) => {
          btnRemove.setAttribute("class", "btnRemove");
 
          for (let element of document.querySelectorAll("form .field")){
+            console.log(element.value);
             element.value = "";
         };
+
+        document.querySelector("form .check").checked = false;
+
         } else {
              warningText.style.display = "block";
              warningText.textContent = "Please, fill all fields!"
@@ -155,7 +180,6 @@ btnNewBook.addEventListener("click", (event) => {
             } 
     
             let tableRowList = bookTable.querySelectorAll("table .table-row");
-    
             for (let i = 0; i < tableRowList.length; i++) {
                 tableRowList[i].index = i;
             }
@@ -167,12 +191,89 @@ btnNewBook.addEventListener("click", (event) => {
     
             for (let i = 0; i < tableRowListLength; i++) {
                 console.log(tableRowList);
+                console.log(event.target.index === tableRowList[i].index);
                 if (event.target.index === tableRowList[i].index){
                     tableRowList[i].remove();
                     myLibrary.splice([i],1);
-            }
                 }
+            };
+
+            if (bookTable.querySelectorAll("table .table-row")[0] === undefined) {
+                firstRowTable.style.display = "none";
+            }
         });
+
+        if (checkConfirmation === "Completed") {
+            optionOne.textContent = "Completed";
+            optionTwo.textContent = "Not finished";
+        } else { 
+            optionOne.textContent = "Not finished";
+            optionTwo.textContent = "Completed";
+        }
+        let optionOneList = bookTable.querySelectorAll("tr .dropdown p:first-child");
+        let optionTwoList = bookTable.querySelectorAll("tr .dropdown p:last-child");
+        let dropdownContainerList = bookTable.querySelectorAll("tr .dropdown-container");
+        let tableRowList = bookTable.querySelectorAll("table .table-row");
+        
+        // Displays dropdown by clicking 
+        dropdown.addEventListener("click", (event) => {
+
+            optionOneList = bookTable.querySelectorAll("tr .dropdown p:first-child");
+            optionTwoList = bookTable.querySelectorAll("tr .dropdown p:last-child");
+            dropdownContainerList = bookTable.querySelectorAll("tr .dropdown-container");
+            tableRowList = bookTable.querySelectorAll("table .table-row");
+
+            for (let i = 0; i < optionOneList.length; i++){
+                optionOneList[i].index = i;
+            } 
+
+            for (let i = 0; i < optionTwoList.length; i++){
+                optionTwoList[i].index = i;
+            } 
+
+            for (let i = 0; i < tableRowList.length; i++){
+                tableRowList[i].index = i;
+            } 
+            
+            
+            for (let i = 0; i < optionOneList.length; i++) {
+                if (event.target.index === optionOneList[i].index){
+                    if (dropdownContainerList[i].style.overflow === "hidden" || dropdownContainerList[i].style.overflow === "") {
+                        function checker(){
+                            if (tableRowList[i+1] === undefined) {
+                                return i;
+                            } else {
+                                return i+1;
+                            }
+                        };
+                        if (Number(tableRowList[checker()].style.zIndex) > tableRowList[i].style.zIndex){
+                            tableRowList[i].style.zIndex = `${Number(tableRowList[checker()].style.zIndex) + 1}`;
+                        } else {
+                            tableRowList[i].style.zIndex = `${Number(tableRowList[i].style.zIndex) + 1}`;
+                        };
+
+                        tableRowList[i].style.position = "relative";
+                        dropdownContainerList[i].style.overflow = "visible";
+                    } else {
+                        tableRowList[i].style.zIndex = "0";
+                        dropdownContainerList[i].style.overflow = "hidden";
+                    }
+                }
+            };
+
+
+        });
+
+        optionTwo.addEventListener("click", (event) => {
+            for (let i = 0; i < optionOneList.length; i++) {
+                if (event.target.index === optionTwoList[i].index) {
+                   optionOneList[i].textContent = optionOneList[i].textContent === "Completed" ? "Not finished" : "Completed";
+                   optionTwoList[i].textContent = optionTwoList[i].textContent === "Not finished" ? "Completed" : "Not finished"; 
+                   myLibrary[i].isRead = optionOne.textContent; 
+                }
+               }
+        })
+        
     })
 
 
