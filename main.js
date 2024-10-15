9c4f96a22f6b9077f4382e38c0bd80bc3957fd2a
metascifi <177558001+metascifi@users.noreply.github.com>
2024-10-15 17:52:12 +0300

    Update: New UI Design

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
 let bookTable = document.querySelector(".book-section table");
 let firstRowTable = document.querySelector("table tr:first-child");
+bookTable.style.display = "none";
 firstRowTable.style.display = "none";
 let contentContainer = document.querySelector(".book-section")
 let btnNewBook = document.querySelector("#newBook");
@@ -10,6 +11,9 @@ let dropdownContainer = document.createElement("div");
 let dropdown = document.createElement("div");
 let optionOne = document.createElement("p");
 let optionTwo = document.createElement("p");
+let inputContainer = document.createElement("div");
+let iconContainer = document.createElement("div");
+let dropdownIcon = document.createElement("img");
 let checkConfirmation;
 
 
@@ -51,6 +55,16 @@ btnNewBook.addEventListener("click", (event) => {
     setAttributes(bookForm, [["method", "get"], ["action", "#"]]);
     contentContainer.appendChild(bookForm);
 
+    let fieldSet = document.createElement("fieldset");
+    bookForm.appendChild(fieldSet);
+
+    let inputContainer = document.createElement("div");
+    inputContainer.setAttribute("class", "input-container");
+    fieldSet.appendChild(inputContainer);
+
+    let nameLabel = document.createElement("label");
+    nameLabel.setAttribute("for", "name");
+    nameLabel.textContent = "Book name";
     let nameInput = document.createElement("input");
     setAttributes(nameInput, [
         ["type", "text"],
@@ -60,8 +74,18 @@ btnNewBook.addEventListener("click", (event) => {
         ["required", ""],
         ["class", "field"]
     ]);
-    bookForm.appendChild(nameInput);
+    inputContainer.appendChild(nameLabel);
+    inputContainer.appendChild(nameInput);
     
+
+    inputContainer = document.createElement("div");
+    inputContainer.setAttribute("class", "input-container");
+    fieldSet.appendChild(inputContainer);
+
+
+    let authorLabel = document.createElement("label");
+    authorLabel.setAttribute("for", "author");
+    authorLabel.textContent = "Author";
     let authorInput = document.createElement("input");
     setAttributes(authorInput, [
         ["type", "text"],
@@ -71,8 +95,17 @@ btnNewBook.addEventListener("click", (event) => {
         ["required", ""],
         ["class", "field"]
     ]);
-    bookForm.appendChild(authorInput);
+    inputContainer.appendChild(authorLabel);
+    inputContainer.appendChild(authorInput);
     
+
+    inputContainer = document.createElement("div");
+    inputContainer.setAttribute("class", "input-container");
+    fieldSet.appendChild(inputContainer);
+
+    let yearLabel = document.createElement("label");
+    yearLabel.setAttribute("for", "year");
+    yearLabel.textContent = "Publishing year";
     let yearInput = document.createElement("input");
     setAttributes(yearInput, [
         ["type", "tel"],
@@ -82,8 +115,16 @@ btnNewBook.addEventListener("click", (event) => {
         ["required", ""],
         ["class", "field"]
     ]);
-    bookForm.appendChild(yearInput);
+    inputContainer.appendChild(yearLabel);
+    inputContainer.appendChild(yearInput);
 
+
+    inputContainer = document.createElement("div");
+    inputContainer.setAttribute("class", "input-container");
+    fieldSet.appendChild(inputContainer);
+    let pagesLabel = document.createElement("label");
+    pagesLabel.setAttribute("for", "pages");
+    pagesLabel.textContent = "Pages";
     let pagesInput = document.createElement("input");
     setAttributes(pagesInput, [
         ["type", "number"],
@@ -93,8 +134,16 @@ btnNewBook.addEventListener("click", (event) => {
         ["required", ""],
         ["class", "field"]
     ]);
-    bookForm.appendChild(pagesInput);
+    inputContainer.appendChild(pagesLabel);
+    inputContainer.appendChild(pagesInput);
 
+
+    inputContainer = document.createElement("div");
+    inputContainer.setAttribute("class", "input-container");
+    bookForm.appendChild(inputContainer);
+    let completeLabel = document.createElement("label");
+    completeLabel.setAttribute("for", "completionCheck");
+    completeLabel.textContent = "Have you read the book?";
     let completeInput = document.createElement('input');
     setAttributes(completeInput, [
         ["type", "checkbox"], 
@@ -104,7 +153,8 @@ btnNewBook.addEventListener("click", (event) => {
         ["value", "on"],
         ["class", "check"]
     ])
-    bookForm.appendChild(completeInput);
+    inputContainer.appendChild(completeLabel);
+    inputContainer.appendChild(completeInput);
     
     btnConfirm = document.createElement('button');
     setAttributes(btnConfirm, [
@@ -117,6 +167,7 @@ btnNewBook.addEventListener("click", (event) => {
     btnConfirm.addEventListener('click', (event) => {
         let canBeAdded = true;
         let btnRemove;
+        let addBookButton;
         
         for (element of document.querySelectorAll("form input")) {
           if (element.value === "") {
@@ -127,6 +178,13 @@ btnNewBook.addEventListener("click", (event) => {
          // Hides table heading and warning text for empty fields;
          firstRowTable.style.display = "table-row";
          warningText.style.display = "none";
+         bookTable.style.display = "block";
+         bookForm.style.display = "none";
+         addBookButton = document.createElement("button");
+         addBookButton.textContent = "Add new book!";
+         addBookButton.setAttribute("class", "add-book-button");
+         contentContainer.appendChild(addBookButton);
+         addBookButton.style.display = "inilne-block";
 
          // Adds a book to the library 
          let addedBook = addBookToLibrary(nameInput.value, authorInput.value, yearInput.value, pagesInput.value, completeInput)
@@ -134,6 +192,7 @@ btnNewBook.addEventListener("click", (event) => {
          tableRow.setAttribute("class", "table-row")
          bookTable.appendChild(tableRow);
          let properties = Object.keys(addedBook);
+         let tableCell;
          for (let i = 0; i < properties.length; i++){
               tableCell = document.createElement("td");
               tableRow.appendChild(tableCell);
@@ -143,20 +202,28 @@ btnNewBook.addEventListener("click", (event) => {
                 tableCell.appendChild(addedBook[properties[i]]);
                 dropdown = document.createElement("div");
                 optionOne = document.createElement("p");
+                iconContainer = document.createElement("div");
+                iconContainer.setAttribute("class", "icon-container");
+                dropdownIcon = document.createElement("img");
+                dropdownIcon.setAttribute("src", "icons/arrow.svg");
                 optionTwo = document.createElement("p");
                 dropdownContainer.setAttribute("class", "dropdown-container");
                 dropdownContainer.appendChild(dropdown);
                 dropdown.setAttribute("class", "dropdown");
                 dropdown.appendChild(optionOne);
                 dropdown.appendChild(optionTwo);
+                optionOne.appendChild(iconContainer);
+                iconContainer.append(dropdownIcon);
                 addedBook[properties[i]] = checkConfirmation;
               }
           };
 
           // Creates remove button 
+         tableCell = document.createElement("td");
+         tableRow.appendChild(tableCell);
          btnRemove = document.createElement("button");
          btnRemove.textContent = "Remove!";
-         tableRow.appendChild(btnRemove);
+         tableCell.appendChild(btnRemove);
          btnRemove.setAttribute("class", "btnRemove");
 
          for (let element of document.querySelectorAll("form .field")){
@@ -169,7 +236,8 @@ btnNewBook.addEventListener("click", (event) => {
         } else {
              warningText.style.display = "block";
              warningText.textContent = "Please, fill all fields!"
-             contentContainer.appendChild(warningText);
+             let body = document.querySelector("body");
+             body.appendChild(warningText);
         }; 
 
         //Removes a book from list 
@@ -199,7 +267,8 @@ btnNewBook.addEventListener("click", (event) => {
             };
 
             if (bookTable.querySelectorAll("table .table-row")[0] === undefined) {
-                firstRowTable.style.display = "none";
+                bookTable.style.display = "none";
+                warningText.style.display = "none";
             }
         });
 
@@ -214,12 +283,14 @@ btnNewBook.addEventListener("click", (event) => {
         let optionTwoList = bookTable.querySelectorAll("tr .dropdown p:last-child");
         let dropdownContainerList = bookTable.querySelectorAll("tr .dropdown-container");
         let tableRowList = bookTable.querySelectorAll("table .table-row");
+        let dropdownList = bookTable.querySelectorAll("tr .dropdown");
         
         // Displays dropdown by clicking 
         dropdown.addEventListener("click", (event) => {
 
             optionOneList = bookTable.querySelectorAll("tr .dropdown p:first-child");
             optionTwoList = bookTable.querySelectorAll("tr .dropdown p:last-child");
+            dropdownList = bookTable.querySelectorAll("tr .dropdown");
             dropdownContainerList = bookTable.querySelectorAll("tr .dropdown-container");
             tableRowList = bookTable.querySelectorAll("table .table-row");
 
@@ -234,7 +305,6 @@ btnNewBook.addEventListener("click", (event) => {
             for (let i = 0; i < tableRowList.length; i++){
                 tableRowList[i].index = i;
             } 
-            
             
             for (let i = 0; i < optionOneList.length; i++) {
                 if (event.target.index === optionOneList[i].index){
@@ -254,9 +324,18 @@ btnNewBook.addEventListener("click", (event) => {
 
                         tableRowList[i].style.position = "relative";
                         dropdownContainerList[i].style.overflow = "visible";
+                        dropdownContainerList[i].style.border = "none";
+                        dropdownList[i].style.border = "2px solid black";
+                        dropdownList[i].style.borderRadius = "8px";
+                        optionTwoList[i].style.display = "block";
+                        dropdownContainerList[i].style.height = "20px";
                     } else {
                         tableRowList[i].style.zIndex = "0";
+                        dropdownContainerList[i].style.border = "2px solid black";
                         dropdownContainerList[i].style.overflow = "hidden";
+                        dropdownContainerList[i].style.height = "initial";
+                        dropdownList[i].style.border = "none";
+                        optionTwoList[i].style.display = "none";                        
                     }
                 }
             };
@@ -274,6 +353,10 @@ btnNewBook.addEventListener("click", (event) => {
                }
         })
         
+        addBookButton.addEventListener("click", ()=> {
+            addBookButton.style.display = "none";
+            bookForm.style.display = "flex";
+        })
     })
 
 
